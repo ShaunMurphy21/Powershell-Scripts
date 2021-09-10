a92e3b09c5d07262ec2771f53cdc276f36861fce
ShaunMurphy21 <shaunplaysGTX@gmail.com>
2021-09-10 19:05:36 +0100

    Add files via upload

--- usefulTools/AddressGen.py
+++ usefulTools/AddressGen.py
@@ -10,9 +10,38 @@ addy = ''
 
 def address_Gen():
 	
-	address1 = ''.join(random.choice(string.digits)for i in range(2))
-	address2 = ''.join(random.choice(chars)for i in range(6))
-	global addy
-	addy = address1 + ' ' + address2.upper() + ' GARDENS'
-	
-address_Gen();
+    address1 = ''.join(random.choice(string.digits)for i in range(2))
+    address2 = ''.join(random.choice(chars)for i in range(6))
+    global addy
+    addy = address1 + ' ' + address2.upper() + ' GARDENS'
+
+    print(addy)
+
+
+for i in range(10):
+    address_Gen();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
